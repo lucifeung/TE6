fe21c4e273a0b68d45fa030c4d0cbbf4462174c8
lucifeung <54411590+lucifeung@users.noreply.github.com>
2022-10-13 22:49:36 -0700

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,11 +2,11 @@
 
 for (i=1; i<=100; i++) {
     if ( !(i%5) && !(i%3) ) {
-        console.log('FizzBuzz');
-    } else if ( !(i%5) && (i%3)) {
-        console.log('Buzz');
+        console.log('FizzBuzz');  //numbers that are divisible by both 3 and 5
+    } else if ( !(i%5)) {
+        console.log('Buzz');  //numbers divisible by 5
     } else if ( !(i%3) ) {
-        console.log('Fizz');
+        console.log('Fizz');  //numbers divisible by 3
     } else {
         console.log(i);
     }
